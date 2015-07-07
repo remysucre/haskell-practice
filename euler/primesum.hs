6df6b38fa1d86ed308c102bd0b0c4a598f8075dd
Yisu Wang <ywang30@cs.tufts.edu>
2015-07-07 18:00:04 -0400

    euler 10

---
+++ euler/primesum.hs
@@ -0,0 +1,11 @@
+import Data.List
+
+primes' :: [Integer]
+primes' = unfoldr nextPrime (1, 2)
+
+nextPrime :: (Integer, Integer) -> Maybe (Integer, (Integer, Integer))
+nextPrime (prd, n)
+    | gcd prd n /= 1 = nextPrime (prd, n + 1)
+    | gcd prd n == 1 = Just (n, (prd * n, n + 1))
+
+ans = sum $ takeWhile (< 2000000) primes'
